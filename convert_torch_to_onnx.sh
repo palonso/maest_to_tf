e72513aba4ab973aa076f84f94f4d41dacca9da1
palonso <pablo.alonso@upf.edu>
2025-01-23 23:17:19 +0100

    Support conversion of all models

--- convert_torch_to_onnx.sh
+++ convert_torch_to_onnx.sh
@@ -3,13 +3,13 @@
 set -e
 
 for model in \
-    # discogs-maest-5s-pw-129e \
-    # discogs-maest-10s-fs-129e \
-    # discogs-maest-10s-dw-75e \
-    # discogs-maest-10s-pw-129e \
-    # discogs-maest-20s-pw-129e \
-    # discogs-maest-30s-pw-129e \
-    # discogs-maest-30s-pw-73e-ts \
+    discogs-maest-5s-pw-129e \
+    discogs-maest-10s-fs-129e \
+    discogs-maest-10s-dw-75e \
+    discogs-maest-10s-pw-129e \
+    discogs-maest-20s-pw-129e \
+    discogs-maest-30s-pw-129e \
+    discogs-maest-30s-pw-73e-ts \
     discogs-maest-30s-pw-129e-519l
 do
     echo processing "$model"

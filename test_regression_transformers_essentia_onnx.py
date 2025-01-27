fdc61adcf201ea723cebe94e8639aa544dde4f11
palonso <pablo.alonso@upf.edu>
2025-01-27 12:57:14 +0100

    Add regression tests

---
+++ test_regression_transformers_essentia_onnx.py
@@ -0,0 +1,105 @@
+from time import time
+import numpy as np
+import torch
+import onnxruntime as ort
+from essentia.standard import TensorflowPredict
+from essentia import Pool
+from transformers import ASTForAudioClassification
+
+
+# Data prep
+np.random.seed(23)
+
+for model_stem in [
+    "discogs-maest-5s-pw-129e",
+    "discogs-maest-10s-fs-129e",
+    "discogs-maest-10s-dw-75e",
+    "discogs-maest-10s-pw-129e",
+    "discogs-maest-20s-pw-129e",
+    "discogs-maest-30s-pw-129e",
+    "discogs-maest-30s-pw-73e-ts",
+    "discogs-maest-30s-pw-129e-519l",
+]:
+
+    print(f"Processing {model_stem}")
+
+    data_len = 0
+    if "5s" in model_stem:
+        data_len = 316
+    elif "10s" in model_stem:
+        data_len = 626
+    elif "20s" in model_stem:
+        data_len = 1256
+    elif "30s" in model_stem:
+        data_len = 1876
+
+    data = np.random.rand(1, 1, data_len, 96).astype("float32")
+
+
+
+    model_ort = f"models_out/{model_stem}.onnx"
+    session = ort.InferenceSession(model_ort)
+
+    # Prepare input
+    inputs = {"melspectrogram": data.squeeze(1)}
+
+    # Run inference
+    output_names = [f"layer_{i:02d}_embeddings" for i in range(0, 12)]
+
+    st = time()
+    outputs_onnx = session.run(output_names, inputs)
+    eta = time() - st
+    print(f"ONNX realtime ETA: {eta:.2f}")
+    outputs_onnx = np.stack([h for h in outputs_onnx]).squeeze()
+    print(f"ONNX output shape: {outputs_onnx.shape}")
+
+    model_tf = f"models_in/{model_stem}/"
+
+    model_trans = ASTForAudioClassification.from_pretrained(model_tf, output_hidden_states=True)
+    model_trans.eval()
+
+    data_pt = torch.Tensor(data).squeeze(1)
+
+    # Trans infer
+    st = time()
+    out_trans = model_trans(data_pt)
+
+    eta = time() - st
+    print(f"Transformers ETA: {eta:.2f}")
+
+    # outputs = out_trans.logits.detach().numpy().squeeze()
+
+    outputs_tf = np.stack([h.detach().numpy().squeeze() for h in out_trans.hidden_states])
+    print(f"Transformers output shapes: {outputs_tf.shape}")
+    # np.save("logits_trans", outputs)
+
+
+    # Model defs
+    inputs = ["melspectrogram"]
+
+    # compare intermediate states
+    outputs = [f"PartitionedCall/Identity_{i}" for i in range(1, 13)]
+
+    model_es = f"models_out/{model_stem}.pb"
+    model = TensorflowPredict(graphFilename=model_es, inputs=inputs, outputs=outputs)
+
+    pool = Pool()
+    pool.set(inputs[0], data)
+
+    # Ess infer
+    st = time()
+    pool_out = model(pool)
+
+    eta = time() - st
+    print(f"Essentia ETA: {eta:.2f}")
+
+    outputs_es = np.stack([pool_out[o] for o in outputs]).squeeze()
+    print(f"Essentia output shapes: {outputs_es.shape}")
+
+    for layer in range(12):
+        print(f"Processing layer {layer}")
+        np.testing.assert_allclose(outputs_es[layer], outputs_tf[layer + 1], rtol=1e-03, atol=1e-03)
+        print("Essentia OK!")
+
+        np.testing.assert_allclose(outputs_onnx[layer], outputs_tf[layer + 1], rtol=1e-03, atol=1e-03)
+        print("ONNX OK!")
